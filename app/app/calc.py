6c5d02cd60d6935a616c7bb966e251806850f9a7
William Boehringer <williamboehringer1@gmail.com>
2023-04-12 12:04:01 -0300

    Updated dependencies and requirements

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""Calculator functions"""
+
+
+def add(x,y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x,y):
+    """Subctract x from y and return result."""
+    return y - x
