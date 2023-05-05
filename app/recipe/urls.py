b594432173cef12a44e4e927ec99fd46cc7792b9
William Boehringer <williamboehringer1@gmail.com>
2023-05-05 12:17:40 -0300

    many updates

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -13,6 +13,8 @@ from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
+router.register('Ingredients', views.IngredientViewset)
 
 app_name = 'recipe'
 
